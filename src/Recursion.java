2ab0ceb83491c6c40d86ba072f157772a577ef40
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2024-08-09 16:47:20 +0500

    Update the recursion program which print five number without any loop and copy of print statmenet.

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,26 @@
 public class Recursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+    public static void print(int n) {
+        System.out.println(n);
+        print1(2);
+    }
+    public static void print1(int n) {
+        System.out.println(n);
+        print2(3);
+    }
+    public static void print2(int n) {
+        System.out.println(n);
+        print3(4);
+    }
+    public static void print3(int n) {
+        System.out.println(n);
+        print4(5);
+
+    }
+    public static void print4(int n) {
+        System.out.println(n);
+
+    }
 }
