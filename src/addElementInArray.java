86536231f5e7d62616680ef5c30b37449d78141d
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2024-08-12 23:07:10 +0500

    This is program to show how you can inserte a element in an array and how to replace them

---
+++ src/addElementInArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class addElementInArray {
+    public static void main(String[] args) {
+        int [] arr=new int[3];
+        Scanner input =new Scanner(System.in);
+        for (int i=0; i<arr.length; i++) {
+            System.out.print("Enter the value at index " + i + " : ");
+            arr[i]=input.nextInt();
+        }
+        System.out.println("Print result: ");
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("Replace the value ");
+        System.out.println("Enter the Index on which to replace the value :");
+        int index=input.nextInt();
+        System.out.print("Enter the value for replace  : ");
+        int replaceValue=input.nextInt();
+
+        if (index>=0&& index<arr.length){
+            arr[index]=replaceValue;
+            System.out.println("After replace the value: ");
+            for (int i=0; i<arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        else {
+            System.out.println("array index out of bound ");
+        }
+
+    }
+}
