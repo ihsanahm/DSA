7e42b1675998ef2ad8d0934f78f170b1d806a7b5
ihsanahm <ihsanahmed.bscsf23@iba-suk.edu.pk>
2024-08-20 17:08:16 +0500

    how to declaer and intialzi 1D  array

---
+++ src/arrayQ1.java
@@ -0,0 +1,10 @@
+
+//How do you declare and initialize a 1D array in Java?"
+
+public class arrayQ1 {
+    public static void main(String[] args) {
+        int [] array;//delaration of array
+        array= new int[7];//initialsiation of array
+
+    }
+}
